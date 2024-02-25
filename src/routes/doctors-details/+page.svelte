05ce8d0f2664563e7b871d5314a39a0d38c72bb1
Lihat Radu <radulihat@gmail.com>
2024-02-25 23:36:36 +0200

    Update +page.svelte

--- src/routes/doctors-details/+page.svelte
+++ src/routes/doctors-details/+page.svelte
@@ -1,13 +1,13 @@
 <div class="text-center">
-  <h1 class="text-5xl font-bold text-indigo-800">Doctor Details List</h1>
+  <h1 class="text-5xl font-bold text-indigo-800 py-20">Doctor Details List</h1>
 </div>
 
-<div id="doctors" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 justify-between grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-<div class="flow-root">
+<div id="doctors" class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 justify-between space-x-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+<div class="flow-root border-black">
   <dl class="-my-3 divide-y divide-gray-100 text-sm">
-    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
+    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4 ">
       <dt class="font-medium text-gray-900">Title</dt>
-      <dd class="text-gray-700 sm:col-span-2">Mr</dd>
+      <dd class="text-gray-700 sm:col-span-2">Doctor</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
@@ -17,12 +17,18 @@
 
     <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
       <dt class="font-medium text-gray-900">Occupation</dt>
-      <dd class="text-gray-700 sm:col-span-2">Guitarist</dd>
+      <dd class="text-gray-700 sm:col-span-2">Dentist</dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
-      <dt class="font-medium text-gray-900">Salary</dt>
-      <dd class="text-gray-700 sm:col-span-2">$1,000,000+</dd>
+      <dt class="font-medium text-gray-900">Profile</dt>
+      <dd class="text-gray-700 sm:col-span-2">
+        <div class="avatar">
+          <div class="w-24 rounded-full">
+            <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+          </div>
+        </div>
+        </dd>
     </div>
 
     <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-3 sm:gap-4">
@@ -31,6 +37,7 @@
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et facilis debitis explicabo
         doloremque impedit nesciunt dolorem facere, dolor quasi veritatis quia fugit aperiam
         aspernatur neque molestiae labore aliquam soluta architecto?
+      <a class="link link-secondary font-bold">See More</a>
       </dd>
     </div>
   </dl>
